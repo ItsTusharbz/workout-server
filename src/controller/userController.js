be6c72e4ab4340b50cbc6861773a85ff873b96d6
Tushar <Tusharbz7276@gmail.com>
2022-01-08 12:12:27 +0530

    added user check and sending 404 if user doesnt exists

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -1,12 +1,32 @@
 const HttpError = require("../model/httpError");
-const { exportData } = require("../utils/util");
+const { exportData, exportError } = require("../utils/util");
 const { con } = require("../utils/db");
+const { USER_DOESNT_EXISTS_CODE } = require("../utils/constants");
+
+const fetchUser = async (userId) => {
+  let sqlquery = "SELECT * from users where 1";
+  if (userId) {
+    sqlquery = "SELECT * from users where id=" + userId;
+  }
+  return new Promise((resolve, reject) => {
+    con.query(sqlquery, (err, result) => {
+      if (err) {
+        console.log(err);
+        reject(err);
+      }
+      resolve(result);
+    });
+  });
+};
 
 const getUsers = async (req, res, next) => {
-  const sqlquery = "SELECT * from users where 1";
-  con.query(sqlquery, (err, result) => {
-    res.send(exportData(result));
-  });
+  const { userId } = req.params;
+  const userData = await fetchUser(userId);
+  if (userData.length) {
+    res.send(exportData(userData));
+  } else {
+    res.send(exportError(USER_DOESNT_EXISTS_CODE, "User doesnt exists"));
+  }
 };
 
 const saveUser = async (req, res, next) => {
@@ -30,21 +50,25 @@ const saveUser = async (req, res, next) => {
 const updateUser = async (req, res, next) => {
   const { userId } = req.params;
   const { name, address, email, contact } = req.body;
-
-  const sqlquery =
-    "UPDATE users SET name = ?, address = ?, email = ?, contact = ? where id = ?";
-  con.query(
-    sqlquery,
-    [name, address, email, contact, userId],
-    (err, result) => {
-      if (err) {
-        const error = new HttpError(err, 500);
-        return next(error);
-      } else {
-        res.send(exportData(result));
+  const userData = await fetchUser(userId);
+  if (userData.length) {
+    const sqlquery =
+      "UPDATE users SET name = ?, address = ?, email = ?, contact = ? where id = ?";
+    con.query(
+      sqlquery,
+      [name, address, email, contact, userId],
+      (err, result) => {
+        if (err) {
+          const error = new HttpError(err, 500);
+          return next(error);
+        } else {
+          res.send(exportData(result));
+        }
       }
-    }
-  );
+    );
+  } else {
+    res.send(exportError(USER_DOESNT_EXISTS_CODE, "user doesnt exists"));
+  }
 };
 
 exports.getUsers = getUsers;
